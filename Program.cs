b773c9c0771c4b2af65fbf1782e835817e61981a
mothnue <dinocruz434@gmail.com>
2025-06-06 18:09:54 +0100

    no longer necessary

--- Program.cs
+++ Program.cs
@@ -51,12 +51,10 @@ void Main()
         Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
         Console.WriteLine(" 1. List all of our current pet information");
         Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-        Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-        Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-        Console.WriteLine(" 5. Edit an animal’s age");
-        Console.WriteLine(" 6. Edit an animal’s personality description");
-        Console.WriteLine(" 7. Display all cats with a specified characteristic");
-        Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+        Console.WriteLine(" 3. Edit an animal’s age");
+        Console.WriteLine(" 4. Edit an animal’s personality description");
+        Console.WriteLine(" 5. Display all cats with a specified characteristic");
+        Console.WriteLine(" 6. Display all dogs with a specified characteristic");
         Console.WriteLine();
         Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
@@ -236,7 +234,7 @@ bool AddAnimalPetList()
 void AnimalAgeDescriptionComplete(string id_animal)
 {
     id_animal = id_animal.ToLower();
-    
+
     if (ourAnimals.ContainsKey(id_animal))
     {
         var animal = ourAnimals[id_animal];
